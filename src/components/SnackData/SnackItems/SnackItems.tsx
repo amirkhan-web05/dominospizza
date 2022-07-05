7ff132e5779136692f7b61bb2dec878037d60d6c
amirkhan_web <amirhanvalman@gmail.com>
2022-07-05 19:13:52 +0300

    refactor snack-items

--- src/components/SnackData/SnackItems/SnackItems.tsx
+++ src/components/SnackData/SnackItems/SnackItems.tsx
@@ -3,14 +3,14 @@ import { TypeSnackItems } from '../../../types'
 
 import styles from './SnackItems.module.scss'
 
-export const SnackItems:React.FC<any> = ({id, x2, name, typePrices, images, onClickAdd, count, price, description}) => {
+export const SnackItems:React.FC<TypeSnackItems> = ({id, name, typePrices, images, onClickAdd, count, price, description}) => {
   return (
     <div className={styles.snacks__items}>
         <img height={224} src={images} alt="" />
         <div className={styles.snacks__info}>
             <h3 className={styles['snacks__info-name']}>{name}</h3>
             <p className={styles['snacks__info-text']}>{description}</p>
-            <button onClick={() => onClickAdd!({id, name, x2, images, price, typePrices:typePrices[0], description, count})} className={styles['snacks__info-price']}>{price} ₽</button>
+            <button onClick={() => onClickAdd!({id, name, images, price, typePrices:typePrices[0], description, count})} className={styles['snacks__info-price']}>{price} ₽</button>
         </div>
     </div>
   )
